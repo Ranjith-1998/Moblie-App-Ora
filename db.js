b1de8b6684a0bf84fcbff39fc4b36a72cb9ae540
Ranjith-1998 <ekambaramranjith@gmail.com>
2025-09-09 13:32:03 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     require: true,
     rejectUnauthorized: false, // Render does not provide CA certs
