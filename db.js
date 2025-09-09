60831698ada20f6386932518c9d73321e8d2cdfb
Ranjith-1998 <ekambaramranjith@gmail.com>
2025-09-09 13:16:53 +0530

    Update db.js

--- db.js
+++ db.js
@@ -4,6 +4,9 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL
+  ssl: {
+    rejectUnauthorized: false,  // needed for Render
+  },
   // OR specify user, host, database, password, port here
 });
 
